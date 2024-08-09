3dddb735b8e17d21ca87a95a4e1d036376bde539
kuldeepKD03 <kuldeepvermakd920@gmail.com>
2024-08-09 21:55:19 +0530

    flask day 2 completed

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask
+from flask import Flask ,render_template
 
 
 app=Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "<h1>welcome to home page!</h1>"
-    
+    return render_template("home.html",title="home")
+
 @app.route("/about")
 def about():
-    return "<h1>welcome to about page</h1>"
+    return render_template("about.html",title="about")
 
-@app.route("/welcome/<name>")
-def welcome(name):
-    return f"<h1>welcome {name.title()} to our we page<h1/>"
+@app.route("/evaluate/<int:num>")
+def evaluate(num):
+    return render_template("evaluate.html",title="evaluate",number=num)
     
 if __name__=="__main__":
     app.run(debug=True)
